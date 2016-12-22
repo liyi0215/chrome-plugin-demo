3fef0ffdb02e5564d9be8a2fb6ec18ac00c79e39
李燚 <ileeyi@163.com>
2016-12-22 18:23:07 +0800

    background->content.script通信

---
+++ background.js
@@ -0,0 +1,47 @@
+function i() {
+    console.info.apply(console, arguments)
+}
+
+chrome.browserAction.onClicked.addListener(function () {
+    i('onClicked - args: ', arguments);
+});
+
+chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
+    i('content.script->background 通讯 - args:', arguments);
+    if (request.greeting == 'hello') {
+        sendResponse({
+            "noInterest": true
+        });
+    } else {
+        sendResponse({});
+    }
+});
+
+chrome.webRequest.onHeadersReceived.addListener(
+    function (response) {
+        var str = 'https://www.baidu.com/home/pcweb/data/mancardwater';
+        if(response.url.indexOf(str) != -1){
+            i('response args:', response, '\n\n\nhahahahahhahahhahahh\n\n\n');
+            // chrome.extension.sendMessage({greeting:"hello. background -> content.script"},function(response){
+            //     i(response, '\n\n\n send content.script response');
+            // });
+            chrome.tabs.query({}, function(tabArr){
+                for(var i = 0, len = tabArr.length; i < len; i++){
+                    if(tabArr[i].url.indexOf('baidu.com') != -1){
+                        chrome.tabs.sendRequest(tabArr[i].id, {noInterest:true})
+                    }
+                }
+            })
+        }
+        return {
+            responseHeaders: response.HttpHeaders
+        }
+    }, {
+        urls: ["<all_urls>"]
+    }, ["blocking"]
+);
+
+
+
+function run() {i('background.js is running...')}
+run();
--- js/index.js
+++ js/index.js
@@ -1,21 +1,20 @@
-$(function(){
-    console.info('jquery-chrome-plugin. body:', $('body'));
-    // var menu = $("#s_menus_wrapper");
-    // if(menu != null){
-    //     menu.append('<i class="fa fa-times-circle" aria-hidden="true" style="font-size:24px;"></i>')
-    // }
-    noInterest();
-    $(document).ajaxComplete(function(){
-        noInterest();
-        console.info('ajaxComplete-chrome-plugin');
-    })
-    function noInterest(){
-        if($('.dustbin')){
-            $('.dustbin').html('不感兴趣').attr("style","width:60px;color:white;background:cornflowerblue;border-radius: 5px;padding: 2px 0;text-align: center;");
-        }
+console.info('jquery-chrome-plugin. body:', $('body'));
+
+function noInterest() {
+    console.count('noInterest is run ...');
+    if ($('.dustbin')) {
+        $('.dustbin').html('不感兴趣').attr("style", "width:60px;color:white;background:cornflowerblue;border-radius: 5px;padding: 2px 0;text-align: center;");
     }
+}
+
+chrome.extension.sendRequest({
+    greeting: "hello"
+}, function (response) {
+    console.info('sendRequest - response: ', response, response.noInterest);
+    if (response.noInterest) noInterest();
 });
 
-chrome.extension.sendRequest({greeting: "hello"}, function(response){
-    console.info('sendRequest - response: ',response);
-});
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
+    if(request.noInterest) noInterest();
+    console.warn('\n\n\n\n\n\n',request, '\n\n\n\n\n\n');
+})
